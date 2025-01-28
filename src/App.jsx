c51daecf3c86bc2c9a0045cf5cae3d402bd195a7
Jerome Mondol <jeromemondol11@gmail.com>
2025-01-28 19:00:04 +0600

    card done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Landing from './components/Landing/Landing';
 import OurPartner from './components/Partner/Partner';
+import Card from './components/Card/Card';
 
 const App = () => {
   return (
     <div>
       <Landing />
       <OurPartner />
+      <Card />
     </div>
   )
 }
--- src/components/Landing/Home.jsx
+++ src/components/Landing/Home.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import SIgnUp from "../Buttons/SIgnUp";
+import HomeImage from "../../assets/image 2-Photoroom.png"
+
+
 const Home = () => {
     return (
-      <div className="Home flex w-full h-[60vh] text-white justify-left items-center bg-[#02242A]">
+      <div className="Home flex w-full h-[60vh] text-white justify-between items-center bg-[#02242A]">
         <div className="Home-part w-fit px-30">
             <h1 className="text-cyan-400 text-2xl">AI SDRs (aiDRs)</h1>
             <h1 className="text-7xl"><span className="font-bold">More</span><span className="italic"> leads,</span> <br/> <span className="font-bold">less</span> <span className="italic"> people.</span></h1>
@@ -10,6 +13,9 @@ const Home = () => {
             <div className="btn mt-5">
             <SIgnUp/>
             </div>
+        </div>
+        <div>
+            <img src={HomeImage} alt="" className="h-[60vh]"/>
         </div>
       </div>
     );
