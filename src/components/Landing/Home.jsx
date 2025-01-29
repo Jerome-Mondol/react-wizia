41b83e2da2ab6f2cff711452c15b733af695dbd5
Jerome Mondol <jeromemondol11@gmail.com>
2025-01-29 23:04:21 +0600

    some chanfes nad

--- src/components/Landing/Home.jsx
+++ src/components/Landing/Home.jsx
@@ -1,28 +1,3 @@
-// import React from "react";
-// import SIgnUp from "../Buttons/SIgnUp";
-// import HomeImage from "../../assets/image 2-Photoroom.png"
-
-
-// const Home = () => {
-//     return (
-//       <div className="Home flex w-full h-[60vh] text-white justify-between items-center bg-[#02242A]">
-//         <div className="Home-part w-fit px-30">
-//             <h1 className="text-cyan-400 text-2xl">AI SDRs (aiDRs)</h1>
-//             <h1 className="text-7xl"><span className="font-bold">More</span><span className="italic"> leads,</span> <br/> <span className="font-bold">less</span> <span className="italic"> people.</span></h1>
-//             <p className="w-96 mt-5 text-gray-400">Train an aiDR on your ICP and messaging matrix. Activate it on a patch. It will send personalized sequences to every target contact.</p>
-//             <div className="btn mt-5">
-//             <SIgnUp/>
-//             </div>
-//         </div>
-//         <div>
-//             <img src={HomeImage} alt="" className="h-[60vh]"/>
-//         </div>
-//       </div>
-//     );
-//   };
-
-//   export default Home;
-
 import React from "react";
 import SIgnUp from "../Buttons/SIgnUp";
 import HomeImage from "../../assets/image 2-Photoroom.png";
--- src/components/Landing/Navbar.jsx
+++ src/components/Landing/Navbar.jsx
@@ -1,38 +1,3 @@
-// import React from 'react'
-// import Logo from "../../assets/Logo.png"
-
-// const Navbar = () => {
-//     const navItems = ["About us", "Pricing", "Customers", "Solutions"]
-//     const navBtns = ["Book a demo", "Contact us"]
-//   return (
-//     <div>
-//         <div className="Navbar flex justify-between items-center px-30 py-5 bg-[#002228] text-white font-mono">
-//             <div className="logo">
-//                 <img src={Logo} alt="" />
-//             </div>
-//             <div className="NavItems flex gap-4">
-//                 {navItems.map((NavItem) => {
-//                     return <a href="#">{NavItem}</a>
-//                 })}
-//             </div>
-//             <div className="NavBtns flex gap-3">
-//                 {navBtns.map((NavBtn, index) => {
-//                     return (
-//                         index === 0 ? <button className="bg-cyan-300 px-5 py-2 rounded-3xl text-black font-bold" >{NavBtn}</button> : <button className="contactBtn border px-5 py-2 rounded-3xl font-bold">{NavBtn}</button>
-//                     )
-//                 })}
-//             </div>
-//         </div>
-//     </div>
-//   )
-// }
-
-// export default Navbar
-
-
-
-
-
 import React, { useState } from "react";
 import Logo from "../../assets/Logo.png";
 import { Menu, X } from "lucide-react"; // Importing icons
