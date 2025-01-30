4dab2fa049902cce5218053c998cc005f480446b
Jerome Mondol <jeromemondol11@gmail.com>
2025-01-30 21:40:24 +0600

    formated code

--- src/components/Effort/Percentages.jsx
+++ src/components/Effort/Percentages.jsx
@@ -21,7 +21,7 @@ const Percentages = () => {
 
   return (
     <div className="flex flex-col md:flex-row justify-center items-center mt-10 gap-6 md:gap-12 px-4 md:px-0">
-      {percentages.map(({ percent, description, id }) => (
+      {percentages.map(({percent,description,id}) => (
         <div className="flex justify-center items-center flex-col gap-2 text-center" key={id}>
           <h1 className="font-bold text-4xl md:text-5xl text-cyan-400">{percent}</h1>
           <p className="text-white text-lg md:text-xl">{description}</p>
